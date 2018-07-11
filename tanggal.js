074b10da1c78a0d4d673b28bcc561800cdc67b02
Kampret <kampretribut@gmail.com>
2018-07-11 22:11:35 +0700

    Update tanggal.js

--- tanggal.js
+++ tanggal.js
@@ -1,9 +1,11 @@
-var waktu=new Date()
-var tahun=waktu.getFullYear()
-var hari=waktu.getDay()
-var bulan=waktu.getMonth()
-var tanggal=waktu.getDate()
-if (tanggal < 7) {tanggal="0"+tanggal} 
-var hariarray=new Array("Minggu","Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu")
-var bulanarray=new Array("Januari","Pebruari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","Nopember","Desember")
-document.write(hariarray[hari]+", "+tanggal+" "+bulanarray[bulan]+"  "+tahun)
+<script type="text/javascript">
+document.write("var waktu=new Date()"); 
+document.writeln("var tahun=waktu.getFullYear()"); 
+document.writeln("var hari=waktu.getDay()"); 
+document.writeln("var bulan=waktu.getMonth()"); 
+document.writeln("var tanggal=waktu.getDate()"); 
+document.writeln("if (tanggal < 7) {tanggal=\"0\"+tanggal} "); 
+document.writeln("var hariarray=new Array(\"Minggu\",\"Senin\",\"Selasa\",\"Rabu\",\"Kamis\",\"Jum'at\",\"Sabtu\")"); 
+document.writeln("var bulanarray=new Array(\"Januari\",\"Pebruari\",\"Maret\",\"April\",\"Mei\",\"Juni\",\"Juli\",\"Agustus\",\"September\",\"Oktober\",\"Nopember\",\"Desember\")"); 
+document.writeln("document.write(hariarray[hari]+\", \"+tanggal+\" \"+bulanarray[bulan]+\"  \"+tahun)");
+</script>
