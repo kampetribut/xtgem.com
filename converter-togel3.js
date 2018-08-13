ea043b9a101bb87a28b0238bcc762cf4a8db8fae
Kampret <kampretribut@gmail.com>
2018-08-13 10:52:24 +0700

    Update converter-togel3.js

--- converter-togel3.js
+++ converter-togel3.js
@@ -1,58 +1,58 @@
-document.write('document.write(" <form id=\"totfrm\" name=\"totfrm\">"); 
-document.writeln("                      <div align=\"center\">"); 
-document.writeln("                        <center>"); 
-document.writeln("                      <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse: collapse\" bordercolor=\"#111111\" width=\"236\" height=\"203\">"); 
-document.writeln("                        <tbody><tr>"); 
-document.writeln("                          <td width=\"450\" height=\"16\" colspan=\"6\" bgcolor=\"#A2A2A2\">"); 
-document.writeln("                          <p style=\"margin-top: 0; margin-bottom: 0\" align=\"center\"><b>"); 
-document.writeln("                          <font face=\"Arial\" size=\"2\" color=\"#FFFFFF\">Winning "); 
-document.writeln("                          Numbers</font></b></p></td>"); 
-document.writeln("                        </tr>"); 
-document.writeln("                        <tr>"); 
-document.writeln("                          <td width=\"40\" height=\"36\" align=\"center\">"); 
-document.writeln("      <input type=\"text\" class=\"inp1\" maxlength=\"2\" onkeyup=\"fCheckNum(this)\" size=\"1\"></td>"); 
-document.writeln("                          <td width=\"40\" height=\"36\" align=\"center\">"); 
-document.writeln("      <input type=\"text\" class=\"inp1\" maxlength=\"2\" onkeyup=\"fCheckNum(this)\" size=\"1\"></td>"); 
-document.writeln("                          <td width=\"40\" height=\"36\" align=\"center\">"); 
-document.writeln("      <input type=\"text\" class=\"inp1\" maxlength=\"2\" onkeyup=\"fCheckNum(this)\" size=\"1\"></td>"); 
-document.writeln("                          <td width=\"40\" height=\"36\" align=\"center\">"); 
-document.writeln("      <input type=\"text\" class=\"inp1\" maxlength=\"2\" onkeyup=\"fCheckNum(this)\" size=\"1\"></td>"); 
-document.writeln("                          <td width=\"40\" height=\"36\" align=\"center\">"); 
-document.writeln("      <input type=\"text\" class=\"inp1\" maxlength=\"2\" onkeyup=\"fCheckNum(this)\" size=\"1\"></td>"); 
-document.writeln("                          <td width=\"40\" height=\"36\" align=\"center\">"); 
-document.writeln("<input type=\"text\" class=\"inp1\" maxlength=\"2\" onkeyup=\"fCheckNum(this)\" size=\"1\"></td>"); 
-document.writeln("                        </tr>"); 
-document.writeln("                        <tr>"); 
-document.writeln("                          <td width=\"450\" height=\"13\" colspan=\"6\" bgcolor=\"#A2A2A2\">"); 
-document.writeln("                          <p align=\"center\"><b>"); 
-document.writeln("                          <font face=\"Arial\" size=\"2\" color=\"#FFFFFF\">Additional "); 
-document.writeln("                          Number</font></b></p></td>"); 
-document.writeln("                        </tr>"); 
-document.writeln("                        <tr>"); 
-document.writeln("                          <td width=\"450\" height=\"35\" colspan=\"6\">"); 
-document.writeln("                                   <p align=\"center\">"); 
-document.writeln("                                   <input class=\"inp1\" maxlength=\"2\" onkeyup=\"fCheckNum(this)\" size=\"1\" name=\"1\"></p></td>"); 
-document.writeln("                        </tr>"); 
-document.writeln("                        <tr>"); 
-document.writeln("                          <td width=\"450\" height=\"16\" colspan=\"6\" bgcolor=\"#A2A2A2\">"); 
-document.writeln("  <p align=\"center\">"); 
-document.writeln("  <b>"); 
-document.writeln("  <font face=\"Arial\" size=\"2\" color=\"#FFFFFF\">4 Digit Results</font></b></p></td>"); 
-document.writeln("                        </tr>"); 
-document.writeln("                        <tr>"); 
-document.writeln("                          <td width=\"450\" height=\"46\" colspan=\"6\">"); 
-document.writeln("                                   <p align=\"center\">"); 
-document.writeln("                                   <span><font face=\"Times New Roman\">"); 
-document.writeln("                                   <input type=\"text\" name=\"Tickt\" readonly=\"true\" size=\"3\" style=\"border:1px solid #FFFFFF; font-family:Arial; font-weight:bold; font-size:14pt; background-color:#A2A2A2; text-align:center; color:#000080\"></font></span></p></td>"); 
-document.writeln("                        </tr>"); 
-document.writeln("                        <tr>"); 
-document.writeln("                          <td width=\"450\" height=\"36\" colspan=\"6\">"); 
-document.writeln("<p style=\"margin-top: 0; margin-bottom: 0\" align=\"center\">"); 
-document.writeln("<tr'><input type=\"button\" name=\"hapus\" id=\"hapus0\" onclick=\"fHapus()\" value=\"Clear\"><font size=\"1\"><font face=\"Arial\"> </font></font></tr'></p></td>"); 
-document.writeln("                        </tr>"); 
-document.writeln("                      </tbody></table>"); 
-document.writeln(""); 
-document.writeln("                        </center>"); 
-document.writeln("                      </div>"); 
-document.writeln(""); 
-document.writeln("</form>");')
+document.write(' <form id="totfrm" name="totfrm">
+                      <div align="center">
+                        <center>
+                      <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" width="236" height="203">
+                        <tbody><tr>
+                          <td width="450" height="16" colspan="6" bgcolor="#A2A2A2">
+                          <p style="margin-top: 0; margin-bottom: 0" align="center"><b>
+                          <font face="Arial" size="2" color="#FFFFFF">Winning 
+                          Numbers</font></b></p></td>
+                        </tr>
+                        <tr>
+                          <td width="40" height="36" align="center">
+      <input type="text" class="inp1" maxlength="2" onkeyup="fCheckNum(this)" size="1"></td>
+                          <td width="40" height="36" align="center">
+      <input type="text" class="inp1" maxlength="2" onkeyup="fCheckNum(this)" size="1"></td>
+                          <td width="40" height="36" align="center">
+      <input type="text" class="inp1" maxlength="2" onkeyup="fCheckNum(this)" size="1"></td>
+                          <td width="40" height="36" align="center">
+      <input type="text" class="inp1" maxlength="2" onkeyup="fCheckNum(this)" size="1"></td>
+                          <td width="40" height="36" align="center">
+      <input type="text" class="inp1" maxlength="2" onkeyup="fCheckNum(this)" size="1"></td>
+                          <td width="40" height="36" align="center">
+<input type="text" class="inp1" maxlength="2" onkeyup="fCheckNum(this)" size="1"></td>
+                        </tr>
+                        <tr>
+                          <td width="450" height="13" colspan="6" bgcolor="#A2A2A2">
+                          <p align="center"><b>
+                          <font face="Arial" size="2" color="#FFFFFF">Additional 
+                          Number</font></b></p></td>
+                        </tr>
+                        <tr>
+                          <td width="450" height="35" colspan="6">
+                                   <p align="center">
+                                   <input class="inp1" maxlength="2" onkeyup="fCheckNum(this)" size="1" name="1"></p></td>
+                        </tr>
+                        <tr>
+                          <td width="450" height="16" colspan="6" bgcolor="#A2A2A2">
+  <p align="center">
+  <b>
+  <font face="Arial" size="2" color="#FFFFFF">4 Digit Results</font></b></p></td>
+                        </tr>
+                        <tr>
+                          <td width="450" height="46" colspan="6">
+                                   <p align="center">
+                                   <span><font face="Times New Roman">
+                                   <input type="text" name="Tickt" readonly="true" size="3" style="border:1px solid #FFFFFF; font-family:Arial; font-weight:bold; font-size:14pt; background-color:#A2A2A2; text-align:center; color:#000080"></font></span></p></td>
+                        </tr>
+                        <tr>
+                          <td width="450" height="36" colspan="6">
+<p style="margin-top: 0; margin-bottom: 0" align="center">
+<tr'><input type="button" name="hapus" id="hapus0" onclick="fHapus()" value="Clear"><font size="1"><font face="Arial"> </font></font></tr'></p></td>
+                        </tr>
+                      </tbody></table>
+
+                        </center>
+                      </div>
+
+</form>'>,
